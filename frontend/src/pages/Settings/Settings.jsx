9a7ec6e98ac05c580260d3d8d219d3fb38bd34b9
Havock48 <adit.github@gmail.com>
2023-11-08 21:55:46 +0100

    setting page mockup added

--- frontend/src/pages/Settings/Settings.jsx
+++ frontend/src/pages/Settings/Settings.jsx
@@ -1,13 +1,65 @@
 import MobileNav from '../../components/MobileNav/MobileNav';
 import Navbar from '../../components/Navbar/Navbar';
+import langImage from '../../assets/bosnian-flag.png';
+import checkImage from '../../assets/check-heart.png';
+import notificationIcon from '../../assets/notification-icon.png';
+import reportIcon from '../../assets/report-icon.png';
+// React icons
+import { RiArrowGoBackLine } from 'react-icons/ri';
+import { MdOutlineBed } from 'react-icons/md';
 import styles from './Settings.module.scss';
 
 export default function Settings() {
     return (
-        <>
+        <div className={styles.settingsWrapper}>
             <Navbar />
-            <h1>Settings</h1>
+            
+            <div className={styles.settings}>
+                {/* GENERAL SETTINGS */}
+                <div className={styles.general}>
+                    <h3>General Settings</h3>
+                    <div className={styles.options}>
+                        <div className={styles.item}>
+                            <img src={langImage} alt="" />
+                            <span>Change language</span>
+                        </div>
+                        <div className={styles.item}>
+                            <img src={checkImage} alt="" />
+                            <p>Change preference</p>
+                        </div>
+                        <div className={styles.item}>
+                            <RiArrowGoBackLine />
+                            <span>Reset preference</span>
+                        </div>
+                        <div className={styles.item}>
+                            <MdOutlineBed />
+                            <span>Disable suggestions</span>
+                        </div>
+                    </div>
+                </div>
+                {/* NOTIFICATION SETTINGS */}    
+                <div className={styles.notifications}>
+                    <h3>Notifications</h3>
+                    <div className={styles.item}>
+                        <img src={notificationIcon} alt="" />
+                        <span>Pop-up notifications</span>
+                    </div>
+                </div>
+                {/* OTHER SETTINGS */}
+                <div className={styles.other}>
+                    <h3>Other</h3>
+                    <div className={styles.item}>
+                        <img src={reportIcon} alt="" />
+                        <span>Report event</span>
+                    </div>
+                    <div className={styles.item}>
+                        <img src={checkImage} alt="" />
+                        <span>Contact Support</span>
+                    </div>
+                </div>
+            </div>
+
             <MobileNav />
-        </>
+        </div>
     )
 }
