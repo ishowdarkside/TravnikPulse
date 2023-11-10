72fe3f46d8d3d881f4ca0a27866bbbced760f5e0
Havock48 <adit.github@gmail.com>
2023-11-10 17:26:30 +0100

    missing parts from last push

--- frontend/src/pages/Shop/PreferenceFilter/PreferenceFilter.jsx
+++ frontend/src/pages/Shop/PreferenceFilter/PreferenceFilter.jsx
@@ -8,8 +8,8 @@ export default function PreferenceFilter() {
 		<section className={styles.preferenceFilter}>
 			{allShopPreferences.map(({ preference, value, icon }, index) => (
 					<div className={styles.preference} key={index} onClick={() => setActiveShopPreference(preference)}>
-						<div className={styles.icon}>{icon}</div>
-						<span className={styles.span}>{value}</span>
+						<div className={activeShopPreference === preference ? `${styles.icon} ${styles.active}` : styles.icon}>{icon}</div>
+						<span className={activeShopPreference === preference ? `${styles.span} ${styles.active}` : styles.span}>{value}</span>
 					</div>
 				))}
 		</section>
