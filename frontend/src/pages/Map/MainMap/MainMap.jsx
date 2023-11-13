4b436b14248fbe474636219d6f2c6889e901246a
ishowdarkside <ajdin.omerovicc.ets@gmail.com>
2023-11-13 22:35:09 +0100

    delete unused vars

--- frontend/src/pages/Map/MainMap/MainMap.jsx
+++ frontend/src/pages/Map/MainMap/MainMap.jsx
@@ -1,68 +1,99 @@
-import { useEffect, useRef } from 'react';
-import { useGetRadiusTours, useGetTours } from '../../../hooks/useTours';
-import { MapContainer, Marker, Popup, TileLayer, useMap, Circle  } from 'react-leaflet';
-import Markers from '../Markers.jsx/Markers';
-import { useGetAllShops, useGetRadiusShops } from '../../../hooks/useShops';
-import styles from './MainMap.module.scss';
-import { FaTimes } from 'react-icons/fa'
-import L from 'leaflet';
-import 'leaflet-routing-machine';
-import 'leaflet/dist/leaflet.css';
-import LeafletRouting from '../LeafletRouting/LeafletRouting';
-import { useMapContext } from '../../../context/MapContext';
+import { useEffect, useRef } from "react";
+import { useGetRadiusTours, useGetTours } from "../../../hooks/useTours";
+import { MapContainer, Marker, TileLayer, Circle } from "react-leaflet";
+import Markers from "../Markers.jsx/Markers";
+import { useGetAllShops, useGetRadiusShops } from "../../../hooks/useShops";
+import styles from "./MainMap.module.scss";
+import { FaTimes } from "react-icons/fa";
+import "leaflet-routing-machine";
+import "leaflet/dist/leaflet.css";
+import LeafletRouting from "../LeafletRouting/LeafletRouting";
+import { useMapContext } from "../../../context/MapContext";
 
 export default function MainMap() {
-	const { activeFilter, setTourLocation, tourLocation, currentPosition, setCurrentPosition } = useMapContext();
-	const { data: tours, isLoading: toursLoading } = useGetTours();
-	const { data: shops, isLoading: shopsLoading } = useGetAllShops();
-	const { data: radiusTours, isLoading: radiusTourLoading } = useGetRadiusTours();
-	const { data: radiusShops, isLoading: radiusShopLoading } = useGetRadiusShops();
-	const cancelRouteRef = useRef();
-	const TRAVNIK_COORS = [ 44.2304, 17.6566 ]
-	useEffect(
-		() => {
-			navigator.geolocation.getCurrentPosition((pos) => {
-				const lat = pos.coords.latitude;
-				const lng = pos.coords.longitude;
-				setCurrentPosition([ lat, lng ]);
-			});
-		},
-		[ setCurrentPosition ]
-	);
+  const {
+    activeFilter,
+    setTourLocation,
+    tourLocation,
+    currentPosition,
+    setCurrentPosition,
+  } = useMapContext();
+  const { data: tours, isLoading: toursLoading } = useGetTours();
+  const { data: shops, isLoading: shopsLoading } = useGetAllShops();
+  const { data: radiusTours, isLoading: radiusTourLoading } =
+    useGetRadiusTours();
+  const { data: radiusShops, isLoading: radiusShopLoading } =
+    useGetRadiusShops();
+  const cancelRouteRef = useRef();
+  const TRAVNIK_COORS = [44.2304, 17.6566];
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((pos) => {
+      const lat = pos.coords.latitude;
+      const lng = pos.coords.longitude;
+      setCurrentPosition([lat, lng]);
+    });
+  }, [setCurrentPosition]);
 
-	if (toursLoading || shopsLoading || radiusTourLoading || radiusShopLoading) return <h1>Loading...</h1>;
+  if (toursLoading || shopsLoading || radiusTourLoading || radiusShopLoading)
+    return <h1>Loading...</h1>;
 
-	const allData = [...radiusTours, ...radiusShops]
-	console.log(allData);
-	const activeData = activeFilter === 'shops' ? shops : activeFilter === 'radius' ? allData : activeFilter === 'tours' ? tours : null;
-	
-	console.log(activeData)
-	console.log(shops)
-	// Check if currentPosition has a valid value before rendering the map
-	if (!currentPosition || currentPosition.length !== 2) return null;
+  const allData = [...radiusTours, ...radiusShops];
 
-	const radiusInKm = 50;
-	const radiusInMeters = radiusInKm * 1000;
+  const activeData =
+    activeFilter === "shops"
+      ? shops
+      : activeFilter === "radius"
+      ? allData
+      : activeFilter === "tours"
+      ? tours
+      : null;
 
-	return (
-		<>
-			<MapContainer id={styles.map} center={TRAVNIK_COORS} zoom={15} scrollWheelZoom={true}>
-			<TileLayer
-				attribution="&copy; <a href=&quot;https://api.mapbox.com/directions/v5&quot;>OpenStreetMap</a> contributors"
-				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-			/>
-			{activeData.map((tour, index) => <Markers key={index} tour={tour} setTourLocation={setTourLocation} />)}
+  // Check if currentPosition has a valid value before rendering the map
+  if (!currentPosition || currentPosition.length !== 2) return null;
 
-			<Marker position={currentPosition} />
-			{activeFilter === 'radius' && <Circle center={currentPosition} radius={radiusInMeters} />}
+  const radiusInKm = 50;
+  const radiusInMeters = radiusInKm * 1000;
 
-			{tourLocation !== null && <button className={styles.cancelRoute} ref={cancelRouteRef} onClick={() => setTourLocation(null)}><FaTimes /></button>}
-			{currentPosition && tourLocation ? (
-				<>
-					<LeafletRouting currentPosition={currentPosition} tourLocation={tourLocation} cancelRouteRef={cancelRouteRef} />
-				</>
-				) : null}
-		</MapContainer>
-		</>
-	);
+  return (
+    <>
+      <MapContainer
+        id={styles.map}
+        center={TRAVNIK_COORS}
+        zoom={15}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://api.mapbox.com/directions/v5">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {activeData.map((tour, index) => (
+          <Markers key={index} tour={tour} setTourLocation={setTourLocation} />
+        ))}
+
+        <Marker position={currentPosition} />
+        {activeFilter === "radius" && (
+          <Circle center={currentPosition} radius={radiusInMeters} />
+        )}
+
+        {tourLocation !== null && (
+          <button
+            className={styles.cancelRoute}
+            ref={cancelRouteRef}
+            onClick={() => setTourLocation(null)}
+          >
+            <FaTimes />
+          </button>
+        )}
+        {currentPosition && tourLocation ? (
+          <>
+            <LeafletRouting
+              currentPosition={currentPosition}
+              tourLocation={tourLocation}
+              cancelRouteRef={cancelRouteRef}
+            />
+          </>
+        ) : null}
+      </MapContainer>
+    </>
+  );
 }
