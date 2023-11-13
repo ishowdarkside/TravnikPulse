e9fdf5c7210039faeca6f0fd9c0cfd546467615c
ishowdarkside <ajdin.omerovicc.ets@gmail.com>
2023-11-14 00:08:31 +0100

    finished map

--- frontend/src/pages/WelcomePage/Location/Location.jsx
+++ frontend/src/pages/WelcomePage/Location/Location.jsx
@@ -2,11 +2,31 @@ import { AiOutlineArrowLeft } from "react-icons/ai";
 import { useTouristDataContext } from "../../../context/TouristDataContext";
 import Map from "../../../components/Map/Map";
 import styles from "./Location.module.scss";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Location() {
-  const { activePanel, setActivePanel, position } = useTouristDataContext();
+  const { activePanel, setActivePanel, position, setPosition } =
+    useTouristDataContext();
 
+  useEffect(() => {
+    // Check if the browser supports geolocation
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          // Successfully retrieved the user's location
+          const { latitude, longitude } = position.coords;
+          setPosition({ lat: latitude, lng: longitude });
+        },
+        (error) => {
+          // Handle errors, such as the user denying permission
+          console.error("Error getting location:", error.message);
+        }
+      );
+    } else {
+      // Geolocation is not supported by the browser
+      console.error("Geolocation is not supported by your browser");
+    }
+  }, []); // Empty dependency array ensures the effect runs once after mount
   return (
     <section className={styles.locationSection}>
       <div className={styles.panel}>
