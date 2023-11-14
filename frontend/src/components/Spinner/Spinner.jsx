449153ea1f346041d3920f6da06618b4b1be5ab8
ishowdarkside <ajdin.omerovicc.ets@gmail.com>
2023-11-14 13:34:23 +0100

    change spinner

--- frontend/src/components/Spinner/Spinner.jsx
+++ frontend/src/components/Spinner/Spinner.jsx
@@ -1,11 +1,11 @@
 import styles from "./Spinner.module.scss";
 import { createPortal } from "react-dom";
-import { SpinnerCircularSplit } from "spinners-react";
+import { SpinnerRoundFilled } from "spinners-react";
 
 export default function Spinner() {
   return createPortal(
     <div className={styles.reactWrapper}>
-      <SpinnerCircularSplit color="#f37022" secondaryColor="#612705" />,
+      <SpinnerRoundFilled color="#f37022" />,
     </div>,
     document.body
   );
