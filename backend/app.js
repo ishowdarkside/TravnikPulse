706b2b11936461f125f65fa56c3a2ed7ae0f2b37
ishowdarkside <ajdin.omerovicc.ets@gmail.com>
2023-11-18 22:24:47 +0100

    protected app from xss and mongodb attacks

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,27 @@
 const express = require("express");
 const path = require("path");
 const dotenv = require("dotenv");
+const helmet = require("helmet");
 const morgan = require("morgan");
 const errorController = require("./controllers/errorController");
 const authRouter = require("./routes/authRoute");
 const tourRouter = require("./routes/tourRoute");
 const reviewRouter = require("./routes/reviewRoute");
 const shopRouter = require("./routes/shopRoute");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 dotenv.config({ path: "./config.env" });
 const app = express();
 
+app.use(helmet()); // Remove $ and . symbols from input
+app.use(xss()); //Remove HTML code from input
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 app.use(morgan("dev"));
+app.use(mongoSanitize());
 
 app.use(express.static(path.join(__dirname, "public")));
 
